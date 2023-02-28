3291a058a99fcbfaa86b94d168137279efb6ceb2
dennis mwaniki <dennomaz@gmail.com>
2023-02-28 13:17:15 +0300

    done assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
   get '/' do
     { message: "Hello world" }.to_json
   end
 
+  get '/games' do
+    Game.all.to_json
+  end
+
+  get '/games/:id' do
+    Game.find(params[:id]).to_json(only: [:title, :genre, :platform, :price], include: {
+      reviews: { only: [:score, :comment], include: { user: { only:[:name] }}}
+      })
+  end
 end
